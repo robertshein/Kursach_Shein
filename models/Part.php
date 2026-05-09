876e288d34ba240e953d658fcc5974859a8aca63
robertshein <robertshein21@gmail.com>
2026-05-09 13:30:08 +0500

    Обновил модели. Шейн

--- models/Part.php
+++ models/Part.php
@@ -6,10 +6,22 @@ class Part {
     public $article;
     public $price;
     public $quantity;
+    public $reserved_quantity;
+    public $min_quantity;
     public $description;
     public $image;
     
-    public function __construct($id, $name, $article, $price, $quantity , $description, $image) {
+    public function __construct(
+        $id,
+        $name,
+        $article,
+        $price,
+        $quantity,
+        $description,
+        $image,
+        $reserved_quantity = 0,
+        $min_quantity = 0
+    ) {
         $this->id = $id;
         $this->name = $name;
         $this->article = $article;
@@ -17,6 +29,16 @@ class Part {
         $this->quantity = $quantity;
         $this->description = $description;
         $this->image = $image;
+        $this->reserved_quantity = $reserved_quantity;
+        $this->min_quantity = $min_quantity;
+    }
+
+    public function getAvailableQuantity() {
+        return max(0, $this->quantity - $this->reserved_quantity);
+    }
+
+    public function isLowStock() {
+        return $this->getAvailableQuantity() <= $this->min_quantity;
     }
 }
 ?>
